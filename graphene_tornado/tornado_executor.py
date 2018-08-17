817ddc040510e470974229e85f92bfe546cfc85b
Joey gracey <joey@yewno.com>
2018-08-17 15:45:43 -0700

    feat(python3): remove non-3 compatible dependency
    remove non-python3 compatible `compiler` dependency and replace `compiler.future.is_future` with
    `tornado.concurrent.is_future`. Rest of project is compatible with python3.7.

--- graphene_tornado/tornado_executor.py
+++ graphene_tornado/tornado_executor.py
@@ -1,8 +1,8 @@
-from compiler.future import is_future
-
 from promise import Promise
 from tornado.gen import convert_yielded, multi_future
 from tornado.ioloop import IOLoop
+from tornado.concurrent import is_future
+
 
 # https://gist.github.com/isi-gach/daef0b34ec5af6f026af52d593131c64
 class TornadoExecutor(object):
